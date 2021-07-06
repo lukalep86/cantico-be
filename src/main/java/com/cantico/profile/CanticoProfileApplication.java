3b2b37ff58ebc6d8a3253e589cf891f01217092c
Luca Lepore <lukalep86@gmail.com>
2021-07-06 15:21:54 +0200

    init cantico-profile project

---
+++ src/main/java/com/cantico/profile/CanticoProfileApplication.java
@@ -0,0 +1,13 @@
+package com.cantico.profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanticoProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanticoProfileApplication.class, args);
+	}
+
+}
