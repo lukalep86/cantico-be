97424a0b8d83a6a9f18b6944b198272eeaa17902
Luca Lepore <lukalep86@gmail.com>
2021-07-09 18:00:57 +0200

    spring security config

---
+++ src/main/java/com/cantico/profile/config/MyBasicAuthenticationEntryPoint.java
@@ -0,0 +1,34 @@
+package com.cantico.profile.config;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.naming.AuthenticationException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyBasicAuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
+
+	
+    public void commence(
+    		
+      HttpServletRequest request, HttpServletResponse response, AuthenticationException authEx) 
+      throws IOException, ServletException {
+        response.addHeader("WWW-Authenticate", "Basic realm=" + getRealmName() + "");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        PrintWriter writer = response.getWriter();
+        writer.println("HTTP Status 401 - " + authEx.getMessage());
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        setRealmName("/**");
+        super.afterPropertiesSet();
+    }
+}
--- src/main/java/com/cantico/profile/config/WebSecurityConfig.java
+++ src/main/java/com/cantico/profile/config/WebSecurityConfig.java
@@ -1,29 +1,59 @@
 package com.cantico.profile.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+    private MyBasicAuthenticationEntryPoint authenticationEntryPoint;
 
-	 protected void configure(HttpSecurity httpSecurity) throws Exception
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+          .withUser("cantico").password(passwordEncoder().encode("cantico"))
+          .authorities("USER");
+    }
 
-	     {
-	         httpSecurity.cors().and().csrf().disable().authorizeRequests()
-	                 .antMatchers().permitAll()
-	                 .anyRequest().authenticated();
-	     }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+          .csrf().disable()
+          .authorizeRequests()
+          .antMatchers("/**").permitAll()
+          .and()
+          .httpBasic()
+          .authenticationEntryPoint(authenticationEntryPoint);
+    }
 
-	     @Bean
-	     CorsConfigurationSource corsConfigurationSource()
-	     {
-	         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	         source.registerCorsConfiguration("/**",new CorsConfiguration().applyPermitDefaultValues());
-	         return (CorsConfigurationSource) source;
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-	     }
+    @Bean
+    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+    	UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    	CorsConfiguration config = new CorsConfiguration();
+    	config.setAllowCredentials(true);
+    	config.addAllowedOrigin("*");
+    	config.addAllowedHeader("*");
+    	config.addAllowedMethod("*");
+    	source.registerCorsConfiguration("/**", config);
 
+    	return source;
+    }
 }
