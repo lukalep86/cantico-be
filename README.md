77bd4d99ade47b424d14cff6c314c64625cfd0a2
Luca Landolfi <l.landolfi@greenvulcano.com>
2021-07-29 14:53:09 +0200

    Add DML.sql script, update README.md and .env

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # cantico-be
 
+## Requisiti
+* CanticoIDM
+* PostgreSQL (lo stesso che usa CanticoIDM)
+
 ## docker
 L'immagine docker può essere personalizzata attraverso le seguenti variabili d'ambiente
 
@@ -18,7 +22,27 @@ Oltre al `Dockerfile`, è fornito anche un `docker-compose.yml` per poter genera
 ```
 docker-compose up --force-recreate --build
 ```
-
-Il file `.env` contiene le variabili d'ambiente usate per configurare l'applicazione, descritte nella sezione precedente, e quelle per configurare il deploy con `docker-compose`
+Il comando legge le variabili di ambiente impostate nel file `.env`. Oltre a quelle usate per configurare l'applicazione, è possibile settare le seguenti variabili per configurare il deploy
 
 * `BE_PORT` Porta esposta dall'applicazione
+
+Viene fornito un file `.env` di esempio con i dati per il deploy sul server Innovaway.
+
+## Note
+Ai fini di test è necessario inserire manualmente delle anagrafiche nel database di IDM, visto che al momento non è deployato il front-end di IDM. Allo scopo è fornito lo script [canticoDML.sql](db/canticoDML.sql), che dovrà essere eseguito sul database configurato in `SPRING_DATASOURCE_URL`.
+
+Sul database presente sul server di deploy 10.10.10.216 le anagrafiche sono già state inserite col suddetto script.
+
+Le mail attive per poter compilare i profili legati alle anagrafiche sono le seguenti:
+
+* luca-cantico@mailinator.com
+* claudio-cantico@mailinator.com
+* ale-cantico@mailinator.com
+* vale-cantico@mailinator.com
+* anto-cantico@mailinator.com
+* nargi-cantico@mailinator.com
+* parisi-cantico@mailinator.com
+* stella-cantico@mailinator.com
+* francesca-cantico@mailinator.com
+
+Per accedere alle caselle di posta, collegarsi a [Mailinator](https://www.mailinator.com/) e inserire la casella di posta desiderata nel campo `Enter Public Mailinator Inbox` e cliccare su `GO`.
---
+++ db/canticoDML.sql
@@ -0,0 +1,78 @@
+INSERT INTO public."Dettagli_Tecnici"
+(id, canale_comunicazione, consenso, frequenza, is_enabled, one_time_password, otp_requested_time)
+VALUES(nextval('"Dettagli_Tecnici_id_seq"'), 'SMS', true, '1', true, '$2a$10$m3XI.w9DVhlz5AJJ9OJoL.AP50ZANrx1f2LE8ScYX5id/xvYrYhWC', '2021-07-09 16:03:07.624');
+INSERT INTO public."Anagrafica"
+(id, cap, cellulare, codicefiscale, cognome, comune, email, indirizzo, nome, "password", provincia, sesso, stato, details_id)
+VALUES(nextval('"Anagrafica_id_seq"'), '84046', '3253321452', 'LKIOPL87G23R567Y', 'LEPORE', 'SALERNO', 'luca-cantico@mailinator.com', 'VIA DEDL CORSO', 'LUCA', '$2a$10$m3XI.w9DVhlz5AJJ9OJoL.AP50ZANrx1f2LE8ScYX5id/xvYrYhWC', 'SALERNO', 'M', 'ITALIA', currval('"Dettagli_Tecnici_id_seq"'));
+
+
+INSERT INTO public."Dettagli_Tecnici"
+(id, canale_comunicazione, consenso, frequenza, is_enabled, one_time_password, otp_requested_time)
+VALUES(nextval('"Dettagli_Tecnici_id_seq"'), 'SMS', true, '1', true, '$2a$10$ujQXTJ2VwcUk.SWnXqRJp.PWJEHh9Eonbttw6nVPu2KUCXgLHG9P.', '2021-07-09 16:03:07.624');
+INSERT INTO public."Anagrafica"
+(id, cap, cellulare, codicefiscale, cognome, comune, email, indirizzo, nome, "password", provincia, sesso, stato, details_id)
+VALUES(nextval('"Anagrafica_id_seq"'), '84046', '3254521452', 'CLBOPL87G23R567Y', 'BOTTONI', 'PONTECAGNANO', 'claudio-cantico@mailinator.com', 'VIA DEL CORSO', 'CLAUDIO', '$2a$10$m3XI.w9DVhlz5AJJ9OJoL.AP50ZANrx1f2LE8ScYX5id/xvYrYhWC', 'SALERNO', 'M', 'ITALIA', currval('"Dettagli_Tecnici_id_seq"'));
+
+
+INSERT INTO public."Dettagli_Tecnici"
+(id, canale_comunicazione, consenso, frequenza, is_enabled, one_time_password, otp_requested_time)
+VALUES(nextval('"Dettagli_Tecnici_id_seq"'), 'SMS', true, '1', true, '$2a$10$m3XI.w9DVhlz5AJJ9OJoL.AP50ZANrx1f2LE8ScYX5id/xvYrYhWC', '2021-07-09 16:03:07.000');
+INSERT INTO public."Anagrafica"
+(id, cap, cellulare, codicefiscale, cognome, comune, email, indirizzo, nome, "password", provincia, sesso, stato, details_id)
+VALUES(nextval('"Anagrafica_id_seq"'), '10100', '3254521452', 'ALEOPL87G23R567Y', 'BUHOCI', 'ROMA', 'ale-cantico@mailinator.com', 'VIA DEL CORSO', 'ALEXANDRU', '$2a$10$m3XI.w9DVhlz5AJJ9OJoL.AP50ZANrx1f2LE8ScYX5id/xvYrYhWC', 'ROMA', 'M', 'ITALIA', currval('"Dettagli_Tecnici_id_seq"'));
+
+
+INSERT INTO public."Dettagli_Tecnici"
+(id, canale_comunicazione, consenso, frequenza, is_enabled, one_time_password, otp_requested_time)
+VALUES(nextval('"Dettagli_Tecnici_id_seq"'), 'SMS', true, '1', true, '$2a$10$m3XI.w9DVhlz5AJJ9OJoL.AP50ZANrx1f2LE8ScYX5id/xvYrYhWC', '2021-07-09 16:03:07.000');
+INSERT INTO public."Anagrafica"
+(id, cap, cellulare, codicefiscale, cognome, comune, email, indirizzo, nome, "password", provincia, sesso, stato, details_id)
+VALUES(nextval('"Anagrafica_id_seq"'), '10560', '3254521452', 'VALOPL87G23R567Y', 'SCOLA', 'BOLOGNA', 'vale-cantico@mailinator.com', 'VIA EMILIA', 'VALENTINA', '$2a$10$m3XI.w9DVhlz5AJJ9OJoL.AP50ZANrx1f2LE8ScYX5id/xvYrYhWC', 'BOLOGNA', 'F', 'ITALIA', currval('"Dettagli_Tecnici_id_seq"'));
+
+
+INSERT INTO public."Dettagli_Tecnici"
+(id, canale_comunicazione, consenso, frequenza, is_enabled, one_time_password, otp_requested_time)
+VALUES(nextval('"Dettagli_Tecnici_id_seq"'), 'SMS', true, '1', true, '$2a$10$m3XI.w9DVhlz5AJJ9OJoL.AP50ZANrx1f2LE8ScYX5id/xvYrYhWC', '2021-07-09 16:03:07.000');
+INSERT INTO public."Anagrafica"
+(id, cap, cellulare, codicefiscale, cognome, comune, email, indirizzo, nome, "password", provincia, sesso, stato, details_id)
+VALUES(nextval('"Anagrafica_id_seq"'), '10560', '3254521452', 'ANTOPL87G23R567Y', 'FRACCHI', 'FIRENZE', 'anto-cantico@mailinator.com', 'VIA PONTE VECCHIO', 'ANTONELLA', '$2a$10$m3XI.w9DVhlz5AJJ9OJoL.AP50ZANrx1f2LE8ScYX5id/xvYrYhWC', 'FIRENZE', 'F', 'ITALIA', currval('"Dettagli_Tecnici_id_seq"'));
+
+
+INSERT INTO public."Dettagli_Tecnici"
+(id, canale_comunicazione, consenso, frequenza, is_enabled, one_time_password, otp_requested_time)
+VALUES(nextval('"Dettagli_Tecnici_id_seq"'), 'SMS', true, '1', true, '$2a$10$m3XI.w9DVhlz5AJJ9OJoL.AP50ZANrx1f2LE8ScYX5id/xvYrYhWC', '2021-07-09 16:03:07.000');
+INSERT INTO public."Anagrafica"
+(id, cap, cellulare, codicefiscale, cognome, comune, email, indirizzo, nome, "password", provincia, sesso, stato, details_id)
+VALUES(nextval('"Anagrafica_id_seq"'), '10560', '3254521452', 'NAROPL87G23R567Y', 'NARGI', 'FIRENZE', 'nargi-cantico@mailinator.com', 'VIA PIAZZA', 'FEDERICA', '$2a$10$m3XI.w9DVhlz5AJJ9OJoL.AP50ZANrx1f2LE8ScYX5id/xvYrYhWC', 'FIRENZE', 'F', 'ITALIA', currval('"Dettagli_Tecnici_id_seq"'));
+
+
+INSERT INTO public."Dettagli_Tecnici"
+(id, canale_comunicazione, consenso, frequenza, is_enabled, one_time_password, otp_requested_time)
+VALUES(nextval('"Dettagli_Tecnici_id_seq"'), 'SMS', true, '1', true, '$2a$10$m3XI.w9DVhlz5AJJ9OJoL.AP50ZANrx1f2LE8ScYX5id/xvYrYhWC', '2021-07-09 16:03:07.000');
+INSERT INTO public."Anagrafica"
+(id, cap, cellulare, codicefiscale, cognome, comune, email, indirizzo, nome, "password", provincia, sesso, stato, details_id)
+VALUES(nextval('"Anagrafica_id_seq"'), '10560', '3254521452', 'PARANT87G23R567Y', 'PARISI', 'NAPOLI', 'parisi-cantico@mailinator.com', 'VIA NUOVA', 'ANTONIO', '$2a$10$m3XI.w9DVhlz5AJJ9OJoL.AP50ZANrx1f2LE8ScYX5id/xvYrYhWC', 'NAPOLI', 'M', 'ITALIA', currval('"Dettagli_Tecnici_id_seq"'));
+
+
+INSERT INTO public."Dettagli_Tecnici"
+(id, canale_comunicazione, consenso, frequenza, is_enabled, one_time_password, otp_requested_time)
+VALUES(nextval('"Dettagli_Tecnici_id_seq"'), 'SMS', true, '1', true, '$2a$10$hUfUNggDJSYJpo.6S/vtOuaTwjorrelto7yK162ObZjyAmtH1M0k.', '2021-07-09 16:03:07.624');
+INSERT INTO public."Anagrafica"
+(id, cap, cellulare, codicefiscale, cognome, comune, email, indirizzo, nome, "password", provincia, sesso, stato, details_id)
+VALUES(nextval('"Anagrafica_id_seq"'), '84046', '3254521452', 'VINOPL87G23R567Y', 'GIOIA', 'ROMA', 'cantico-profile-test@mailinator.com', 'VIA DEL CORSO', 'VINCENZO', '$2a$10$5nM0gB14kS3AfA7QunfH4OVStYiaaOOBThp2TXTceeFNjI7319WAe', 'ROMA', 'M', 'ITALIA', currval('"Dettagli_Tecnici_id_seq"'));
+
+
+INSERT INTO public."Dettagli_Tecnici"
+(id, canale_comunicazione, consenso, frequenza, is_enabled, one_time_password, otp_requested_time)
+VALUES(nextval('"Dettagli_Tecnici_id_seq"'), 'SMS', true, '1', true, '$2a$10$U1o1wUNqLB75QxLoUXm5luyvHbVOAkQslTw/6RqKHEF49mpIEUSRG', '2021-07-09 16:03:07.624');
+INSERT INTO public."Anagrafica"
+(id, cap, cellulare, codicefiscale, cognome, comune, email, indirizzo, nome, "password", provincia, sesso, stato, details_id)
+VALUES(nextval('"Anagrafica_id_seq"'), '10500', '3254521452', 'MAROPL87G23R567Y', 'STELLA', 'NAPOLI', 'stella-cantico@mailinator.com', 'CORSO UMBERTO', 'MARTINA', '$2a$10$m3XI.w9DVhlz5AJJ9OJoL.AP50ZANrx1f2LE8ScYX5id/xvYrYhWC', 'NAPOLI', 'F', 'ITALIA', currval('"Dettagli_Tecnici_id_seq"'));
+
+
+INSERT INTO public."Dettagli_Tecnici"
+(id, canale_comunicazione, consenso, frequenza, is_enabled, one_time_password, otp_requested_time)
+VALUES(nextval('"Dettagli_Tecnici_id_seq"'), 'SMS', true, '1', true, '$2a$10$19ecbukZyR3bsT14pv.PKeAQ.N2AhAw9g2LqdP63kddBURoTstZvS', '2021-07-11 16:03:07.624');
+INSERT INTO public."Anagrafica"
+(id, cap, cellulare, codicefiscale, cognome, comune, email, indirizzo, nome, "password", provincia, sesso, stato, details_id)
+VALUES(nextval('"Anagrafica_id_seq"'), '10500', '3254521452', 'FRAOPL87G23R567Y', 'SANTORINI', 'MILANO', 'francesca-cantico@mailinator.com', 'VIA NAZIONALE', 'FRANCESCA', '$2a$10$H4zFRMBR8hgb356TdLj/L.DQ.0LVe18rOdeyM6iz6a4KxJ8O9F4XG', 'MILANO', 'F', 'ITALIA', currval('"Dettagli_Tecnici_id_seq"'));
